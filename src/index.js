231b611338b91cb292b5a9f77d1f48a41c161a17
tanya-sharma05 <tanyasharma7658@gmail.com>
2026-02-15 00:59:29 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import express from "express";
 import urlRouter from "./routes/url.routes.js";
 import {connectDB} from "./db/index.js";
 import dotenv from "dotenv";
+import { Url } from "./models/url.models.js";
 
 dotenv.config();
 const app= express();
 const PORT= process.env.PORT || 8000; 
+
+app.set('view engine', 'ejs');
+app.set("views", "./src/views");
+
+app.get('/test', async(req,res)=>{
+    const allUrls= await Url.find({});
+    return res.render('main',{
+        urls: allUrls
+    });
+})
 
 await connectDB();
 app.use(express.json());
